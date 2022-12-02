46691652072ed3f8af686e6282b9de26d8ebecf9
Adel <576853-astran@users.noreply.gitlab.com>
2022-12-02 21:09:07 +0100

    d1 data

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,7 +1,20 @@
 class Day01(path: String = "day01/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Long {
-        return 0
+        var index =0
+        var sum=0L
+        var values = mutableListOf<Long>()
+        inputData.forEach{v->
+            if(v.isEmpty()){
+                values.add(sum)
+                index++
+                sum=0
+            }else{
+                sum +=v.toLong()
+            }
+
+        }
+        return values.maxOf { it }
     }
     fun compute2(): Long {
         return 0
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -7,13 +7,13 @@ class Day01Test {
     @Test
     fun test_compute_should_be_X() {
         val day = Day01("day01/inputTest")
-        assertEquals(0, day.compute())
+        assertEquals(24000, day.compute())
     }
 
     @Test
     fun compute_should_be_X() {
         val day = Day01("day01/input")
-        assertEquals(0, day.compute())
+        assertEquals(69693, day.compute())
     }
     @Test
     fun test2_compute_should_be_X() {
